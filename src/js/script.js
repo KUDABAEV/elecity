5e972ac759b4413739f3070b51a4ed8713aeea3c
user <arturo.kudabaev@mail.ru>
2026-05-15 00:59:39 +0500

    style(scss/header): set up the project finished the header layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-@@include('alert.js')
+@@include('jquery-3.6.0.min.js')
+@@include('slick.js')
+
+$(function() {
+    $('.index-slider__wrapper-bg').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+});
+})
